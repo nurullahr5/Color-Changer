760516a1c4f46d7862ee55985228fb08fafe1e8c
Md Nurullah <151961025+nurullahr5@users.noreply.github.com>
2023-12-15 18:47:26 +0600

    Add files via upload

---
+++ NRcode.js
@@ -0,0 +1,34 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach( (button) => {
+    console.log(button);
+    button.addEventListener('click', (event) => {
+        console.log(event);
+        console.log(event.target);
+        if(event.target.id === "gray"){
+            body.style.backgroundColor = event.target.id;
+            body.style.color = "white"
+        }
+        if(event.target.id === "red"){
+            body.style.backgroundColor = event.target.id
+            body.style.color = "white"
+        }
+        if(event.target.id === "blue"){
+            body.style.backgroundColor = event.target.id
+            body.style.color = "white"
+        }
+        if(event.target.id === "purple"){
+            body.style.backgroundColor = event.target.id
+            body.style.color = "white"
+        }
+        if(event.target.id === "yellow"){
+            body.style.backgroundColor = event.target.id
+            body.style.color = "black"
+        }
+        if(event.target.id === "green"){
+            body.style.backgroundColor = event.target.id
+            body.style.color = "white"
+        }
+    })
+} )
